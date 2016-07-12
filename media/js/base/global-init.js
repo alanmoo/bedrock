432a7d0518a0f63e84338e4223f17d161b546d3f
Hector Zhao <bzhao@mozilla.com>
2016-07-12 14:17:45 +0800

    Bug 1264843: link to China build of Fx4A, for display within Fx China repack

--- media/js/base/global-init.js
+++ media/js/base/global-init.js
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* global initDownloadLinks, initMobileDownloadLinks, initLangSwitcher */
+/* global initDownloadLinks, initMobileDownloadLinks, initLangSwitcher,
+   maybeSwitchToDistDownloadLinks */
 
 // init global.js functions
 $(document).ready(function() {
@@ -11,5 +12,11 @@ $(document).ready(function() {
     initLangSwitcher();
     $(window).on('load', function () {
         $('html').addClass('loaded');
+
+        /* Bug 1264843: In partner distribution of desktop Firefox, switch the
+           downloads to corresponding partner build of Firefox for Android. */
+        if (Mozilla.Client.isFirefoxDesktop) {
+            Mozilla.Client.getFirefoxDetails(maybeSwitchToDistDownloadLinks);
+        }
     });
 });
